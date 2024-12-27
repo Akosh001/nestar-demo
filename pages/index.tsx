bb075df6a4fb0f109514089032ff2b393b39372f
Akosh001 <makhsudovakhrorbek@gmail.com>
2024-12-27 17:50:59 +0900

    feat: meta tags integrations on document private file

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,16 +39,7 @@ export default function Home() {
           </div>
         </div>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
+        <div className={styles.center}>"Hello MIT"</div>
 
         <div className={styles.grid}>
           <a
